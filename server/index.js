735bace1568b26ade72dc00632ff21c6be2e5ce2
chenmichael2 <chenmichael2012@gmail.com>
2022-04-01 21:13:40 -0700

    allowing req.body and cors

--- server/index.js
+++ server/index.js
@@ -3,3 +3,12 @@ const app = express();
 const cors = require('cors');
 
 //middleware
+
+app.use(express.json()) //allows req.body
+app.use(cors());
+
+//ROUTES//
+
+app.listen(5000, () => {
+    console.log("server is running on port 5000");
+})
